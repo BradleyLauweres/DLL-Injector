fd0cfd52e58943985b312ee8f7791e977c4fb502
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2024-09-02 11:22:56 +0200

    Add ProcessSnapshot

--- DLLInjector.cpp
+++ DLLInjector.cpp
@@ -1,20 +1,30 @@
-// DLLInjector.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+DWORD GetProcessId(const char* procName) 
+{
+	DWORD procId = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+
+		if (Process32First(hSnap, &procEntry)) {
+			do {
+				if (! _stricmp(procEntry.szExeFile, procName)) {
+
+					procId = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnap, &procEntry));
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
