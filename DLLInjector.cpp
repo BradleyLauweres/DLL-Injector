9820f179ae4223ea4827208fdc65d5c48deebff4
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2024-09-03 11:22:09 +0200

    retrieve procceslist

--- DLLInjector.cpp
+++ DLLInjector.cpp
@@ -20,10 +20,33 @@ DWORD GetProcessId(const char* procName)
 			} while (Process32Next(hSnap, &procEntry));
 		}
 	}
+
+	CloseHandle(hSnap);
+	return procId;
 }
 
 int main()
 {
-   
+	const char* pathTodll = "";
+
+	std::cout << "Running tasklist command to retrieve "
+		"list of running processes:"
+		<< std::endl;
+
+	int returnValue = system("tasklist");
+
+	if (returnValue != 0) {
+		std::cerr << "Error executing tasklist command" << std::endl;
+		return 1;
+	}
+
+	int PIDToInject = 0;
+	std::cout << "Please enter the PID from the procces u want to inject to" << std::endl;
+	std::cin >> PIDToInject;
+
+	return 0;
+
+
+
 }
 
